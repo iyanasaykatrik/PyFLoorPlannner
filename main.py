4ebedcbf8512a19af06cadd5191b77f6983242be
Iyana Saykat Rik <iyanasaykat005@gmail.com>
2026-09-11 18:11:47 +0530

    Automated the room adding annd checking for valid input process

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
 plot_x=float(input("Enter length of plot: "))
 plot_y=float(input("Enter breadth of plot: "))
-room_x1=float(input("Enter length of room1: "))
-room_y1=float(input("Enter breadth of room1: "))
-room_x2=float(input("Enter length of room2: "))
-room_y2=float(input("Enter breadth of room2: "))
-start_x1=float(input("Enter the starting x1-coordinate: "))
-start_y1=float(input("Enter the starting y1-coordinate: "))
-start_x2=float(input("Enter the starting x2-coordinate: "))
-start_y2=float(input("Enter the starting y2-coordinate: "))
+
+#STORING INFO OF ROOMS
+data_rooms=[]
+no_of_rooms=int(input("Enter the number of rooms: "))
+i=1
+while i<=no_of_rooms:
+    Roomi=[]
+    print("FOR ROOM",i)
+    room_x=float(input("Enter length of room: "))
+    Roomi.append(room_x)
+    room_y=float(input("Enter breadth of room: "))
+    Roomi.append(room_y)
+    start_x=float(input("Enter the starting x-coordinate: "))
+    Roomi.append(start_x)
+    start_y=float(input("Enter the starting y-coordinate: "))
+    Roomi.append(start_y)
+    data_rooms.append(Roomi)
+    i+=1
+    print(data_rooms)
+
+#CHECKING FOR INVALID INPUTS AND ADDING ROOMS TO THE PLOT
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
-fig, ax=plt.subplots()
-plot=Rectangle((0,0),plot_x,plot_y,fill=False)
-room1=Rectangle((start_x1,start_y1),room_x1,room_y1,fill=False)
-room2=Rectangle((start_x2,start_y2),room_x2,room_y2,fill=False)
-ax.add_patch(plot)
+fig,ax=plt.subplots()
+
+for room in data_rooms:
+    inside_plot=room[2]+room[0]<=plot_x and room[3]+room[1]<=plot_y and room[2]>=0 and room[3]>=0
+    valid_input=room[0]>0 and room[1]>0
+    if inside_plot and valid_input:
+        Room=Rectangle((room[2],room[3]),room[0],room[1],fill=False)
+        ax.add_patch(Room)
+    else:
+        print("Room doesn't fit inside plot or invalid input")    
 
 #CHECK FOR OVERLAP
 
-def check_overlap(room1,room2):
-    s1x,s1y=room1.get_x(),room1.get_y()
-    x1,y1=room1.get_width(),room1.get_height()
-    s2x,s2y=room2.get_x(),room2.get_y()
-    x2,y2=room2.get_width(),room2.get_height()
+def check_overlap():
+    for i in range(0,no_of_rooms-1):
+        for j in range(i+1,no_of_rooms):
+            overlap_x=data_rooms[i][2]+data_rooms[i][0]>data_rooms[j][2] and data_rooms[j][2]+data_rooms[j][0]>data_rooms[i][2]
+            overlap_y=data_rooms[i][3]+data_rooms[i][1]>data_rooms[j][3] and data_rooms[j][3]+data_rooms[j][1]>data_rooms[i][3]
+            if overlap_x and overlap_y:
+                print("The rooms overlap")
+                return True
+            
+    print("The rooms doesn't overlap")
+    return False
 
-    overlap_x=s1x+x1>s2x and s2x+x2>s1x
-    overlap_y=s1y+y1>s2y and s2y+y2>s1y
-    if overlap_x and overlap_y:
-        print("The room overlaps")
-        return True
-    else:
-        print("The room doesn't overlap")
-        return False
-
-#CHECKING IF ROOM INSIDE PLOT
-if (start_x1+room_x1)<=plot_x and (start_y1+room_y1)<=plot_y and (start_x2+room_x2)<=plot_x and (start_y2+room_y2)<=plot_y and 0<=start_x1<=plot_x and 0<=start_y1<=plot_y and 0<=start_x2<=plot_x and 0<=start_y2<=plot_y and start_x1>=0 and start_y1>=0 and start_x2>=0 and start_y2>=0 and room_x1>0 and room_y1>0 and room_x2>0 and room_y2>0 and plot_x>0 and plot_y>0 :
-    ax.add_patch(room1)
-    ax.add_patch(room2)
-    check_overlap(room1,room2)
-
-else:
-    print("room doesnt fit inside the plot or invalid dimensions")
-
+check_overlap()
 ax.set_xlim(0,plot_x)
 ax.set_ylim(0,plot_y)
 ax.set_aspect("equal")
