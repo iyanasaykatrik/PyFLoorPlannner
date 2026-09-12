101da2c39e41265aec4222e95860355bfec37d00
Iyana Saykat Rik <iyanasaykat005@gmail.com>
2026-09-12 21:29:40 +0530

    Separated the check for valid layout and plotting the rooms process.Added labels to the rooms

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ i=1
 while i<=no_of_rooms:
     Roomi=[]
     print("FOR ROOM",i)
+    room_type=input("Enter room type: ")
+    Roomi.append(room_type)
     room_x=float(input("Enter length of room: "))
     Roomi.append(room_x)
     room_y=float(input("Enter breadth of room: "))
@@ -20,7 +22,7 @@ while i<=no_of_rooms:
     i+=1
     print(data_rooms)
 
-#CHECKING FOR INVALID INPUTS AND ADDING ROOMS TO THE PLOT
+#CHECKING FOR INVALID INPUTS
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
@@ -28,14 +30,12 @@ from matplotlib.patches import Rectangle
 fig,ax=plt.subplots()
 def check_rooms_valid():
     for room in data_rooms:
-        inside_plot=room[2]+room[0]<=plot_x and room[3]+room[1]<=plot_y and room[2]>=0 and room[3]>=0
-        valid_input=room[0]>0 and room[1]>0
-        if inside_plot and valid_input:
-            Room=Rectangle((room[2],room[3]),room[0],room[1],fill=False)
-            ax.add_patch(Room)
-        else:
-            print("All rooms doesn't fit inside plot")
+        inside_plot=room[3]+room[1]<=plot_x and room[4]+room[2]<=plot_y and room[3]>=0 and room[4]>=0
+        valid_input=room[1]>0 and room[2]>0
+        if not(inside_plot and valid_input):
+            print("Invalid room position or dimension")
             return False
+    print("All the inputs are valid")
     return True
 
 #CHECK FOR OVERLAP
@@ -43,20 +43,24 @@ def check_rooms_valid():
 def check_overlap():
     for i in range(0,no_of_rooms-1):
         for j in range(i+1,no_of_rooms):
-            overlap_x=data_rooms[i][2]+data_rooms[i][0]>data_rooms[j][2] and data_rooms[j][2]+data_rooms[j][0]>data_rooms[i][2]
-            overlap_y=data_rooms[i][3]+data_rooms[i][1]>data_rooms[j][3] and data_rooms[j][3]+data_rooms[j][1]>data_rooms[i][3]
+            overlap_x=data_rooms[i][3]+data_rooms[i][1]>data_rooms[j][3] and data_rooms[j][3]+data_rooms[j][1]>data_rooms[i][3]
+            overlap_y=data_rooms[i][4]+data_rooms[i][2]>data_rooms[j][4] and data_rooms[j][4]+data_rooms[j][2]>data_rooms[i][4]
             if overlap_x and overlap_y:
                 print("Rooms ",i+1," and ",j+1,"overlap")
                 return False                
     
     return True
 
-#RENDERING THE ROOMS
- 
+#ADDING ROOMS TO PLOT AND RENDERING THE ROOMS
 if check_rooms_valid() and check_overlap():
-    ax.set_xlim(0,plot_x)
-    ax.set_ylim(0,plot_y)
-    ax.set_aspect("equal")
-    plt.show()
+    for room in data_rooms:
+        Room=Rectangle((room[3],room[4]),room[1],room[2],fill=False)
+        ax.add_patch(Room)
+        ax.text(room[3]+room[1]/2,room[4]+room[2]/2,room[0],ha="center",va="center")
+    else:
+        ax.set_xlim(0,plot_x)
+        ax.set_ylim(0,plot_y)
+        ax.set_aspect("equal")
+        plt.show()
 else:
     print("Invalid layout")
